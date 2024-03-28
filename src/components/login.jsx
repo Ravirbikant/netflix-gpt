40a50e7ccb736433c458a668ab31813f5a6ff559
ravikant.jha@voizo.ai <ravikant.jha@voizo.ai>
2024-03-28 23:30:18 +0530

    Skeletal login and sign up form added

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
   return (
     <div>
       <Header />
+      <form>
+        <div className="d-flex flex-column w-50 border m-auto my-2">
+          <h1>{isSignUp ? "Sign Up" : "Sign In"}</h1>
+          {isSignUp && <input type="text" placeholder="Name" />}
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign In</button>
+          <p
+            onClick={() => {
+              setIsSignUp(!isSignUp);
+            }}
+          >
+            {!isSignUp
+              ? "New User? Click here to Sign in"
+              : "Already a user click here."}
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
