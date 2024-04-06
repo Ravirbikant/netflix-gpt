6f2b94f98d14ee77380f53aaae26ade3daec6d26
ravikant.jha@voizo.ai <ravikant.jha@voizo.ai>
2024-04-06 22:42:59 +0530

    Email and password validation added

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { validateEmailPassword } from "../utils/validate";
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  function handleSignInClick() {
+    let validateTest = validateEmailPassword(
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(validateTest);
+  }
+
   return (
     <div>
       <Header />
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <div className="d-flex flex-column w-50 border m-auto my-2">
           <h1>{isSignUp ? "Sign Up" : "Sign In"}</h1>
           {isSignUp && <input type="text" placeholder="Name" />}
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button>Sign In</button>
+          <input ref={email} type="text" placeholder="Email" />
+          <input ref={password} type="password" placeholder="Password" />
+          <p>{errorMessage}</p>
+          <button onClick={handleSignInClick}>Sign In</button>
           <p
             onClick={() => {
               setIsSignUp(!isSignUp);
