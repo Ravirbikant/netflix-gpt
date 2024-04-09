e44f3e089c49afc68985578edec2ca5274c9a500
ravikant.jha@voizo.ai <ravikant.jha@voizo.ai>
2024-04-09 18:11:13 +0530

    Unsubscribed to onAuthStateChanged when component unmounts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
   }
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName, photoURL } = user;
         dispatch(
@@ -37,6 +37,9 @@ const Header = () => {
         navigate("/");
       }
     });
+
+    // Unsubscribe to onAuthStateChanged when Header component unmounts
+    return () => unsubscribe();
   }, []);
 
   return (
