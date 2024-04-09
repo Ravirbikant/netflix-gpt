3954eebb7d484ea718dd3d4907aa14535db4e023
ravikant.jha@voizo.ai <ravikant.jha@voizo.ai>
2024-04-09 19:11:28 +0530

    Successful API call

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-3752-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzIzNDBlMzMzM2M0MzlhYTIzMjdiMDNiYWE5NTZkYSIsInN1YiI6IjY2MTUzYzBiYTZhNGMxMDE4NmJlZTVlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bjoRhQ0KLpkuY1vthmpN96Dnp9rSH1XlEZYc41YZWGg",
+  },
+};
