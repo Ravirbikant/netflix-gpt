6d02629704b72ec20ad125d2e235af886e8f8a36
ravikant.jha@voizo.ai <ravikant.jha@voizo.ai>
2024-04-11 09:12:32 +0530

    Got movie trailer using Youtube key, stored trailer value in Redux and created hook to get movie trailer

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
 const VideoContainer = ({ movieId }) => {
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    );
-    const json = await data.json();
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
 
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData[0];
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
-  return <div>Video Container</div>;
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoContainer;
